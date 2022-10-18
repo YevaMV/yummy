03ea47b7b40b44e6609f304ebc6bffb27e1634b9
YevaMV <eva280988@gmail.com>
2022-10-18 17:43:44 +0400

    added fake data

---
+++ src/assets/fake-data/products.js
@@ -0,0 +1,86 @@
+import image_01 from '../images/image1.png';
+
+const products = [
+  {
+    id: '01',
+    title: 'Accessorize 01',
+    price: 5000,
+    image: image_01,
+    category: 'necklace',
+    description: 'Color - white, size 45cm',
+  },
+
+  {
+    id: '02',
+    title: 'Accessorize 02',
+    price: 5000,
+    image: image_01,
+    category: 'necklace',
+    description: 'Color - white, size 45cm',
+  },
+
+  {
+    id: '03',
+    title: 'Accessorize 03',
+    price: 5000,
+    image: image_01,
+    category: 'necklace',
+    description: 'Color - white, size 45cm',
+  },
+
+  {
+    id: '04',
+    title: 'Accessorize 04',
+    price: 5000,
+    image: image_01,
+    category: 'necklace',
+    description: 'Color - white, size 45cm',
+  },
+
+  {
+    id: '05',
+    title: 'Accessorize 05',
+    price: 3000,
+    image: image_01,
+    category: 'bracelet',
+    description: 'Color - blue, size 16cm',
+  },
+
+  {
+    id: '06',
+    title: 'Accessorize 06',
+    price: 3000,
+    image: image_01,
+    category: 'bracelet',
+    description: 'Color - blue, size 16cm',
+  },
+
+  {
+    id: '07',
+    title: 'Accessorize 07',
+    price: 3000,
+    image: image_01,
+    category: 'bracelet',
+    description: 'Color - blue, size 16cm',
+  },
+
+  {
+    id: '08',
+    title: 'Accessorize 08',
+    price: 3000,
+    image: image_01,
+    category: 'bracelet',
+    description: 'Color - blue, size 16cm',
+  },
+
+  {
+    id: '09',
+    title: 'Accessorize 09',
+    price: 6000,
+    image: image_01,
+    category: 'sunglass chain',
+    description: 'Color - black, size 80cm',
+  },
+];
+
+export default products;
